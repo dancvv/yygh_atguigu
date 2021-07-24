ec18a48fff292ff5177f571103010d10007b0c47
MountainWang <uquantum@hotmail.com>
2021-07-24 16:57:39 +0800

    实现部分方法

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/mapper/HospitalSetMapper.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/mapper/HospitalSetMapper.java
@@ -3,5 +3,17 @@ package com.atguigu.yygh.hosp.mapper;
 import com.atguigu.yygh.model.hosp.HospitalSet;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.io.Serializable;
+
 public class HospitalSetMapper extends BaseMapper<HospitalSet> {
+
+    @Override
+    public int insert(HospitalSet entity) {
+        return 0;
+    }
+
+    @Override
+    public int deleteById(Serializable id) {
+        return 0;
+    }
 }
