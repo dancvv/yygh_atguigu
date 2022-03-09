c7c50e3b4f40520e5d3063f1ae1d145c13a0044b
server <uquantum@hotmail.com>
2022-03-09 04:42:17 +0000

    deleteall

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/HospitalSetService.java
+++
@@ -1,7 +0,0 @@
-package com.atguigu.yygh.hosp.service;
-
-import com.atguigu.yygh.model.hosp.HospitalSet;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface HospitalSetService extends IService<HospitalSet> {
-}
